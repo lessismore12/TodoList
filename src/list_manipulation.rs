ac8916879f44318d61950b0579a617f6eb2aefae
Andrew Less <andrew.less22@gmail.com>
2024-01-15 19:43:45 -0500

    adding a single word item works and so does reafing to and from the file

--- src/list_manipulation.rs
+++ src/list_manipulation.rs
@@ -1,39 +1,107 @@
-use std::{fs::{File, OpenOptions}, path::Path, io::{Read, Write}};
+use std::{fs::{File, OpenOptions}, path::Path, io::{Read, Write, BufWriter}};
+
 
 #[allow(unused)]
 const FILE_PATH: &str = "todo.txt";
 
+pub struct TodoList {
+    file: File,
+    file_path: String,
+}
+
+impl TodoList {
+
+    pub fn new() -> Self {
+        if does_file_exist() {
+            Self {
+                file: File::open(FILE_PATH)
+                .expect(&format!("file not found: {}", FILE_PATH)),
+                file_path: FILE_PATH.to_string()
+            }
+        } else {
+            println!("I see you don't have a list started. I'll make one for you");
+            Self {
+                file: File::create(FILE_PATH).expect("Failed to create file"),
+                file_path: FILE_PATH.to_string()
+            }
+        }
+    }
+
+    pub fn add_item(&mut self, items: &[String]) { 
+        let file = OpenOptions::new().append(true).open(self.file_path.clone()).unwrap();
+        let mut buffer = BufWriter::new(file);
+        for item in items {
+            let line = format!("{}\n", item);
+            let _ = buffer.write_all(line.as_bytes());
+        }
+       
+    }
+
+    pub fn create_file(&mut self) {
+        self.file = File::create(FILE_PATH).expect("Failed to create file");
+    }
+
+    pub fn read_from_file(&self) {
+        let mut file = File::open(FILE_PATH)
+            .expect(&format!("file not found: {}", FILE_PATH));
+    
+        let mut contents = String::new();
+    
+        file.read_to_string(&mut contents)
+            .expect(&format!("cannot read file {}", FILE_PATH));
+    
+        println!("{}", contents);
+    }
+
+    #[allow(unused)]
+    fn get_contents(&mut self) {
+    
+        let mut contents = String::new();
+    
+        self.file.read_to_string(&mut contents)
+            .expect(&format!("cannot read file {}", FILE_PATH));
+    
+        for line in contents.lines() {
+            print!("{:?}", line)
+        }
+    }
+
+    const TODO_HELP: &'static str = "Usage: todo [COMMAND] [ARGUMENTS]
+        Todo is a super fast and simple tasks organizer written in rust
+        Example: todo list
+        Available commands:
+            - add [TASK/s]
+                adds new task/s
+                Example: todo add \"buy carrots\"
+            - list
+                lists all tasks
+                Example: todo list
+            - done [INDEX]
+                marks task as done
+                Example: todo done 2 3 (marks second and third tasks as completed)
+            - rm [INDEX]
+                removes a task
+                Example: todo rm 4
+            - reset
+                deletes all tasks
+            - restore 
+                restore recent backup after reset
+            - sort
+                sorts completed and uncompleted tasks
+                Example: todo sort
+            - raw [todo/done]
+                prints nothing but done/incompleted tasks in plain text, useful for scripting
+                Example: todo raw done
+        ";
+    pub fn help() {
+        // For readability
+        println!("{}", Self::TODO_HELP);
+    }
+    
+}
+
+
 #[allow(unused)]
 fn does_file_exist() -> bool {
     Path::new(FILE_PATH).exists()
-}
-
-#[allow(unused)]
-pub fn create_file() -> File {
-    let file = File::create(FILE_PATH).expect("Failed to create file");
-    file
-}
-
-#[allow(unused)]
-pub  fn add_item(list: String, item: String) -> String {
-    [list, item].join("\n")
-}
-
-#[allow(unused)]
-pub fn read_from_file(file_path: &str) -> String {
-    let mut file = File::open(file_path)
-        .expect(&format!("file not found: {}", file_path));
-
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents)
-        .expect(&format!("cannot read file {}", file_path));
-
-    contents
-}
-
-#[allow(unused)]
-pub fn write_to_file(file_path: &str) {
-    let mut file = OpenOptions::new().append(true).open(file_path).unwrap();
-    file.write("buf\n".as_bytes()).expect(&format!("cannot write to file {}", file_path));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
-use std::io::{stdin, stdout, Write};
-
+// use std::io::{stdin, stdout, Write};
 mod list_manipulation;
+//use list_manipulation::TodoList;
+
+// fn main() {
+//     let mut s = String::new();
+//     print!("Let's make a todo list for you. Please enter in an item: ");
+//     let _ = stdout().flush();
+//     stdin().read_line(&mut s).expect("Did not enter a correct string");
+
+//     println!("You typed: {}",s);
+//     let new_file = list_manipulation::create_file();
+    
+// }
+
+use std::env;
+
+use crate::list_manipulation::TodoList;
 
 fn main() {
-    let mut s = String::new();
-    print!("Let's make a todo list for you. Please enter in an item: ");
-    let _ = stdout().flush();
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
 
-    println!("You typed: {}",s);
-    let new_file = list_manipulation::create_file();
-    
+    let mut todo_list = TodoList::new();
+
+    let args: Vec<String> = env::args().collect();
+
+    println!("{:?}", args);
+
+    println!("{:?}", &args[1]);
+    if args.len() >= 1 { 
+        let command: &String = &args[1];
+
+        match &command[..] {
+            "create" => todo_list.create_file(),
+            "add" => todo_list.add_item(&args[2..]),
+            "--help" => TodoList::help(),
+            &_ => todo_list.read_from_file()
+        } 
+    } else {
+        TodoList::help();
+    }
 }
